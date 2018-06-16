2a052a26dbe87f538061ab95373312e4abdcccfa
Michael Babker <michael.babker@gmail.com>
2018-06-16 10:21:26 -0500

    Improvements to announcements module

--- templates/joomla/html/mod_articles_news/announcements.php
+++ templates/joomla/html/mod_articles_news/announcements.php
@@ -9,54 +9,58 @@
 
 defined('_JEXEC') or die;
 
-$item_heading = $params->get('item_heading', 'h4');
 ?>
 <div class="newsflash<?php echo $moduleclass_sfx; ?>">
 	<?php foreach ($list as $item) : ?>
-		<?php if ($params->get('item_title')) : ?>
-			<<?php echo $item_heading; ?> class="newsflash-title<?php echo $params->get('moduleclass_sfx'); ?>">
-			<?php if ($params->get('link_titles') && $item->link != '') : ?>
-				<a href="<?php echo $item->link; ?>">
-					<?php echo $item->title; ?>
-				</a>
-			<?php else : ?>
-				<?php echo $item->title; ?>
+		<div class="newsflash-item">
+			<?php if ($params->get('item_title')) : ?>
+				<h3 class="newsflash-title<?php echo $params->get('moduleclass_sfx'); ?>">
+					<?php if ($params->get('link_titles') && $item->link != '') : ?>
+						<a href="<?php echo $item->link; ?>">
+							<?php echo $item->title; ?>
+						</a>
+					<?php else : ?>
+						<?php echo $item->title; ?>
+					<?php endif; ?>
+				</h3>
 			<?php endif; ?>
-			</<?php echo $item_heading; ?>>
-		<?php endif; ?>
 
-		<?php if (!$params->get('intro_only')) : ?>
-			<?php echo $item->afterDisplayTitle; ?>
-		<?php endif; ?>
+			<?php if (!$params->get('intro_only')) : ?>
+				<?php echo $item->afterDisplayTitle; ?>
+			<?php endif; ?>
 
-		<?php echo $item->beforeDisplayContent; ?>
+			<?php echo $item->beforeDisplayContent; ?>
 
-		<?php
-			// strip tags and cleanup whitespace
-			$text = strip_tags($item->introtext);
-			$text = preg_replace('|  +|', ' ', $text);
+			<?php
+				// strip tags and cleanup whitespace
+				$text = strip_tags($item->introtext);
+				$text = preg_replace('|  +|', ' ', $text);
 
-			// word limit check
-			$max_words = (int) 35;
-			$words = explode(' ', $text);
+				// word limit check
+				$max_words = (int) 35;
+				$words = explode(' ', $text);
 
-			if (count($words) > $max_words) {
-				$words = array_splice($words, 0, $max_words);
-				$text = implode(' ', $words);
-				$text .= '...';
-			} else {
-				$text = implode(' ', $words);
-			}
-		?>
+				if (count($words) > $max_words)
+				{
+					$words = array_splice($words, 0, $max_words);
+					$text = implode(' ', $words);
+					$text .= '...';
+				}
+				else
+				{
+					$text = implode(' ', $words);
+				}
+			?>
 
-		<div class="newsflash-body"><?php echo $text; ?></div>
+			<div class="newsflash-body"><?php echo $text; ?></div>
 
-		<?php if (isset($item->link) && $item->readmore != 0 && $params->get('readmore')) : ?>
-			<p class="readmore">
-				<a class="btn" href="<?php echo $item->link; ?>" itemprop="url">
-					<span class="icon-chevron-right"></span> <?php echo $item->linkText; ?>
-				</a>
-			</p>
-		<?php endif; ?>
+			<?php if (isset($item->link) && $item->readmore != 0 && $params->get('readmore')) : ?>
+				<div class="readmore">
+					<a class="btn" href="<?php echo $item->link; ?>" itemprop="url">
+						<span class="icon-chevron-right"></span> <?php echo $item->linkText; ?>
+					</a>
+				</div>
+			<?php endif; ?>
+		</div>
 	<?php endforeach; ?>
 </div>
